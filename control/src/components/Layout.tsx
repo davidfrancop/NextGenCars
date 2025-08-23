dc2b98b028c837784936315552dac4abdad0f3ab
David Franco <dfrancop@gmail.com>
2025-08-23 20:17:02 +0200

    Actualizar Layout.tsx

--- control/src/components/Layout.tsx
+++ control/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 // src/components/Layout.tsx
 
-import { ReactNode } from "react"
+import type { ReactNode } from "react"
 import Sidebar from "./Sidebar"
 
 type Props = { children?: ReactNode }
@@ -8,13 +8,20 @@ type Props = { children?: ReactNode }
 export default function Layout({ children }: Props) {
   return (
     <div className="bg-gray-900 text-white min-h-screen">
-      {/* Sidebar fijo/overlay */}
-      <Sidebar />
+      {/* Desktop: sidebar estático empuja contenido con flex
+          Mobile: sidebar es overlay; el contenido ocupa todo */}
+      <div className="flex">
+        <div className="hidden md:block">
+          <Sidebar />
+        </div>
 
-      {/* Contenido: en desktop deja espacio para el sidebar; en móvil ocupa todo */}
-      <main className="min-h-screen md:pl-64 p-6">
-        {children}
-      </main>
+        {/* Mobile: también necesitamos el Sidebar montado para overlay */}
+        <div className="md:hidden">
+          <Sidebar />
+        </div>
+
+        <main className="flex-1 p-6">{children}</main>
+      </div>
     </div>
   )
 }
