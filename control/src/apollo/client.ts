ba156ec1189e8376790e3e4fe213470abda54fb7
David Franco <dfrancop@gmail.com>
2025-08-25 21:12:03 +0200

    Update some Archive 2

--- control/src/apollo/client.ts
+++ control/src/apollo/client.ts
@@ -1,10 +1,18 @@
-// src/apollo/client.ts
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+// control/src/apollo/client.ts
 
-const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
-  cache: new InMemoryCache(),
+import { ApolloClient, InMemoryCache, from, HttpLink } from "@apollo/client"
+import { onError } from "@apollo/client/link/error"
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    console.error(`[GraphQL @ ${operation.operationName}]`, graphQLErrors)
+    console.error("Query:", operation.query?.loc?.source.body)
+    console.error("Vars:", operation.variables)
+  }
+  if (networkError) console.error("[Network]", networkError)
 })
 
-export default client
-
+export default new ApolloClient({
+  link: from([errorLink, new HttpLink({ uri: import.meta.env.VITE_API_URL })]),
+  cache: new InMemoryCache(),
+})
